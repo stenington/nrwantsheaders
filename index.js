9abd8b73768b4cdb0abb039d1fa7674864b27471
Mike Larsson <mikelarssonftw@gmail.com>
2016-09-26 21:00:12 -0400

    some log formatting

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var request = require('request');
 
 var configs = []
 for (var key in process.env) {
-  if (key.indexOf('NEW_RELIC_APP_ID_') == 0) {
-    var match = key.match(/^NEW_RELIC_APP_ID_(\d+)$/);
+  if (key.indexOf('WEBHOOK_PATH_') == 0) {
+    var match = key.match(/^WEBHOOK_PATH_(\d+)$/);
     var idx = match[1];
 
     var app_id = process.env['NEW_RELIC_APP_ID_' + idx];
@@ -18,7 +18,7 @@ for (var key in process.env) {
         key: key,
         webhook_path: path
       });
-      console.log('Config parsed for index', idx);
+      console.log(`Config parsed for WEBHOOK_PATH_${idx}=${path}`);
     }
   }
 }
@@ -33,7 +33,7 @@ for (var idx in configs) {
   var key = config.key;
 
   app.post('/' + path, function (req, res, next) {
-    console.log('heroku ->', req.body);
+    console.log('heroku ->\n', JSON.stringify(req.body, null, 2));
 
     var opts = {
       url: 'https://api.newrelic.com/v2/applications/' + app_id + '/deployments.json',
@@ -48,9 +48,9 @@ for (var idx in configs) {
         }
       }
     }
-    console.log('-> new relic', opts);
+    console.log('new relic ->\n', JSON.stringify(opts, null, 2));
     request.post(opts, function (err, res, body) {
-      console.log('new relic <-', err, body);
+      console.log('new relic <-\n', JSON.stringify({err: err, body: body}, null, 2));
     });
 
     res.status(200).send('OK');
